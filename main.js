57240ea0f746ea530a03a17b264766a551588424
William Paiva <williampaivabet@outlook.com>
2023-04-05 20:27:37 -0300

    First Commit]

---
+++ main.js
@@ -0,0 +1,28 @@
+let contador = 1
+
+setInterval(function(){
+    document.getElementById("slide" + contador).checked = 1
+    contador++
+    if(contador>4)
+        contador = 1
+}, 4000)
+
+const theme = document.getElementById('check')
+const bg = document.getElementById('a')
+
+theme.addEventListener('change', () => {
+    
+    if(theme.checked){
+        bg.classList.add('dark_theme')
+        bg.classList.remove('light_theme') 
+    }else{   
+        bg.classList.add('light_theme')
+        bg.classList.remove('dark_theme') 
+    }
+})
+
+
+
+
+
+
